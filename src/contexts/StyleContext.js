ce1e57de1b02d53914ac2b494169edca514f0c00
borys914 <yassin.borys.dev@gmail.com>
2021-10-15 08:26:20 +0800

    blog component color update

--- src/contexts/StyleContext.js
+++ src/contexts/StyleContext.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const StyleContext = React.createContext();
-
+ 
 export const StyleProvider = StyleContext.Provider;
 export const StyleConsumer = StyleContext.Consumer;
 
